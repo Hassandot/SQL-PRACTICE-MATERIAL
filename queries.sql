61250a0b7da647d60ab207be2630834d54bba7f7
Hassan Tanveer <hassant1035@gmail.com>
2026-04-15 03:40:38 +0500

    dont start with vowel

--- queries.sql
+++ queries.sql
@@ -8,7 +8,7 @@ select CITY,length(CITY) from STATION order by length(CITY) DESC,CITY LIMIT 1;
 select city from STATION where CITY regexp '^[aeiouAEIOU]';-- select city from STATION where CITY regexp '^[aeiouAEIOU]';-
 select distinct CITY from STATION where CITY regexp '[aeiouAEIOU]$'; -- select distinct CITY from STATION where CITY regexp '[aeiouAEIOU]$'; --
 select distinct CITY from STATION where CITY regexp '^[aeiouAEIOU].*[aeiouAEIOU]$';
-
+select distinct CITY from STATION where CITY regexp '^[^aeiouAEIOU]';
 
 
 
