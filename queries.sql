777569c34895190cdac3acb96cf6a16f5f9b988a
Hassan Tanveer <hassant1035@gmail.com>
2026-04-15 02:52:29 +0500

    shortest,longest

--- queries.sql
+++ queries.sql
@@ -2,4 +2,6 @@ SELECT * from users where id = 1;
 SELECT * from CITY where COUNTRYCODE='JPN';
 select NAME from CITY where COUNTRYCODE='JPN';
 SELECT CITY,STATE from STATION;
-select count(CITY) - count(distinct CITY) from STATIO
+select count(CITY) - count(distinct CITY) from STATION;
+select CITY,length(CITY) from STATION order by length(CITY),CITY LIMIT 1;
+select CITY,length(CITY) from STATION order by length(CITY) DESC,CITY LIMIT 1;
