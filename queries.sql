36af266b82de21ab282a69035591d6797b985a11
Hassan Tanveer <hassant1035@gmail.com>
2026-04-15 03:35:33 +0500

    first vowel

--- queries.sql
+++ queries.sql
@@ -5,3 +5,4 @@ SELECT CITY,STATE from STATION;
 select count(CITY) - count(distinct CITY) from STATION;
 select CITY,length(CITY) from STATION order by length(CITY),CITY LIMIT 1;
 select CITY,length(CITY) from STATION order by length(CITY) DESC,CITY LIMIT 1;
+select city from STATION where CITY regexp '^[aeiouAEIOU]';-- select city from STATION where CITY regexp '^[aeiouAEIOU]';-
